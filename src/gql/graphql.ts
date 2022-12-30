d1983349a480159e49eca322ffb44c87d4325d2e
shibamoto <silverbirder@gmail.com>
2022-12-30 11:10:33 +0900

    codegen

---
+++ src/gql/graphql.ts
@@ -0,0 +1,34 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type GuestbookEntry = {
+  __typename?: 'GuestbookEntry';
+  message: Scalars['String'];
+  name: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  entries?: Maybe<Array<GuestbookEntry>>;
+};
+
+export type GetEntriesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetEntriesQuery = { __typename?: 'Query', entries?: Array<{ __typename?: 'GuestbookEntry', name: string, message: string }> | null };
+
+
+export const GetEntriesDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"getEntries"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"entries"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"message"}}]}}]}}]} as unknown as DocumentNode<GetEntriesQuery, GetEntriesQueryVariables>;
